53f914f8c5b0d01851c5de41ddca51a749d3db78
iiio2 <gomexxxx71@gmail.com>
2021-04-19 16:07:47 +0600

    added set window width.

--- src/hooks/UseEffect.jsx
+++ src/hooks/UseEffect.jsx
@@ -1,27 +1,29 @@
 import { Fragment, useState, useEffect } from 'react';
 
 const UseEffect = () => {
-  const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  console.log('render');
+  //   useEffect(() => {
+  //     console.log('done');
+  //     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //       .then((response) => response.json())
+  //       .then((json) => setItems(json));
+  //   }, [resourceType]);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
 
   useEffect(() => {
-    console.log('done');
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <Fragment>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-      <h3>{resourceType}</h3>
-      {items.map((item) => (
-        <pre>{JSON.stringify(item)}</pre>
-      ))}
+      <h4>{windowWidth}</h4>
     </Fragment>
   );
 };

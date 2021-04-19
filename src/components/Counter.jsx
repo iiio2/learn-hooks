d932e92fe70b37a6ae4dd38bfaccac02febf9f13
iiio2 <gomexxxx71@gmail.com>
2021-04-19 13:49:33 +0600

    added objects in useHook part

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,28 @@
 import { Fragment, useState } from 'react';
 
 const Counter = () => {
-  const init = () => {
-    console.log('run every time');
-    return 4;
-  };
+  const [state, setState] = useState({ count: 4, theme: 'blue' });
 
-  const [counter, setCounter] = useState(() => init());
+  const count = state.count;
+  const theme = state.theme;
 
   const dec = () => {
-    setCounter((prevState) => prevState - 1);
-    setCounter((prevState) => prevState - 1);
-    setCounter((prevState) => prevState - 1);
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 2 };
+    });
+  };
+
+  const chThm = () => {
+    setState((prevState) => {
+      return { ...prevState, theme: 'red' };
+    });
   };
 
   return (
     <Fragment>
       <button>+</button>
-      <h4>{counter}</h4>
+      <h4>{count}</h4>
+      <h4 onClick={chThm}>{theme}</h4>
       <button onClick={dec}>-</button>
     </Fragment>
   );
